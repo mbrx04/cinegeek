2db0cbce7dd3cc2ac6cc291542dd7688ae1d4aaa
ruffoloemilio <rffmle04r16d086y@studenti.unical.it>
2025-12-16 16:21:43 +0100

    unione file momentanei

---
+++ lib/UI/widgets/cineGlassTextField.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CineGlassTextField extends StatelessWidget {
+  final String hint;
+  final bool obscure;
+
+  const CineGlassTextField({
+    super.key,
+    required this.hint,
+    this.obscure = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: Colors.black.withAlpha(90),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(
+            color: Colors.white.withAlpha(26),
+          ),
+        ),
+        child: TextField(
+          obscureText: obscure,
+          style: textTheme.bodyMedium,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: textTheme.labelMedium,
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
